762860fb7cccd9fd3fbc13dcc6bc12afee2444ae
kazusa0920 <kazusaneco0920@gmail.com>
2025-11-30 16:27:13 +0900

    できた！…けどまだあんまり理解していない。。

--- JavaScript/2025-11/2025-11-29/todo-app01/script.js
+++ JavaScript/2025-11/2025-11-29/todo-app01/script.js
@@ -2,7 +2,8 @@ const todoForm = document.querySelector('form');
 const todoInput = document.getElementById('todo-input');
 const todoListUL = document.getElementById('todo-list');
 
-let allTodos = [];
+let allTodos = getTodos();
+updateTodoList()
 
 todoForm.addEventListener('submit', function (e) {
     e.preventDefault();
@@ -10,15 +11,77 @@ todoForm.addEventListener('submit', function (e) {
 })
 
 function addTodo() {
-    const todoTest = todoInput.value.trim();
+    const todoTest = todoInput.value.trim(); //ユーザーが入力した情報を余分な空白を除いた状態で「todoTest」とする
     if (todoTest.length > 0) {
-        allTodos.push(todoTest);
-        createTodoItem(todotest);
-        todoInput.value = '';
+        const todoObject = {
+            text: todoTest,
+            completed: false
+        }
+        allTodos.push(todoObject); //あらかじめ作っているallTodosの配列に「todoTest」を追加する
+        updateTodoList();
+        saveTodos();
+        todoInput.value = ''; //追加したらinputの中を空にする
     }
 }
-function createTodoItem(todo) {
+
+function updateTodoList() {
+    todoListUL.innerHTML = '';
+    allTodos.forEach((todo, todoIndex) => {
+        todoItem = createTodoItem(todo, todoIndex);
+        todoListUL.append(todoItem);
+    })
+}
+
+function createTodoItem(todo, todoIndex) {
+    const todoId = 'todo-' + todoIndex;
     const todoLI = document.createElement('li');
-    todoLI.innerText = todo;
-    todoListUL.append(todoLI);
+    const todoTest = todo.text;
+    todoLI.className = 'todo';
+    todoLI.innerHTML = `
+                <input type="checkbox" id="${todoId}">
+
+                <label for="${todoId}" class="custom-checkbox">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="transparent">
+                        <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" />
+                    </svg>
+                </label>
+
+                <label for="${todoId}" class="todo-test">
+                    ${todoTest}
+                </label>
+
+                <button class="delete-button">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="var(--secondary-color)">
+                        <path
+                            d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
+                    </svg>
+                </button>
+    `
+    const deleteButton = todoLI.querySelector('.delete-button')
+    deleteButton.addEventListener('click', () => {
+        deleteTodoItem(todoIndex)
+    })
+    const checkbox = todoLI.querySelector('input');
+    checkbox.addEventListener('change', () => {
+        allTodos[todoIndex].completed = checkbox.checked;
+        saveTodos();
+    })
+    checkbox.checked = todo.completed;
+    return todoLI;
+}
+
+function deleteTodoItem(todoIndex) {
+    allTodos = allTodos.filter((_, i) => i !== todoIndex);
+    saveTodos();
+    updateTodoList()
+}
+
+function saveTodos() {
+    const todosJson = JSON.stringify(allTodos);
+    localStorage.setItem('todos', todosJson);
+}
+
+function getTodos() {
+    const todos = localStorage.getItem('todos') || '[]';
+    return JSON.parse(todos);
 }
