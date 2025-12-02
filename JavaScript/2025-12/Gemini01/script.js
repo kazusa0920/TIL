b67cbd3eb6a68b067896a3e508ab97f979219cc2
kazusa0920 <kazusaneco0920@gmail.com>
2025-12-03 04:38:14 +0900

    む、むずかしい…。

---
+++ JavaScript/2025-12/Gemini01/script.js
@@ -0,0 +1,15 @@
+const taskList = document.getElementById('task-list');
+const addButton = document.getElementById('add-button');
+
+let task02 = document.getElementById('task-2')
+task02.textContent = 'プログラミングの勉強をする'
+
+let task01 = document.getElementById('task-1')
+task01.classList.add('highlight')
+
+let task03 = document.getElementById('task-3')
+task03.classList.add('completed')
+
+addButton.addEventListener('click', () => {
+    
+})
